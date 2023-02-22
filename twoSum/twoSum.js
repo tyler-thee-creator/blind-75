510164e8b9ce55c7ded225a025bfe844465f4781
Tyler Brown <tyler.s.brown.93@gmail.com>
2023-02-22 13:24:42 -0800

    initialize twoSum

---
+++ twoSum/twoSum.js
@@ -0,0 +1,13 @@
+/*https://leetcode.com/problems/two-sum/*/
+
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+
+const twoSum = (nums, target) => {
+  // code
+}
+
+export default twoSum;
