18f9b122319b30db7701f16d42869b312539c48f
Tyler Brown <tyler.s.brown.93@gmail.com>
2023-02-22 13:48:46 -0800

    solve twoSum

--- twoSum/twoSum.js
+++ twoSum/twoSum.js
@@ -7,7 +7,13 @@
  */
 
 const twoSum = (nums, target) => {
-  // code
+  const map = new Map();
+
+  for (let i = 0; i < nums.length; i++) {
+    if (map.has(target - nums[i])) return [i, map.get(target - nums[i])];
+
+    map.set(nums[i], i);
+  }
 }
 
 export default twoSum;
