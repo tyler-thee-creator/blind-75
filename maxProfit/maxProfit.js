e424d8e57d2458446a362b5e9c0fae1b682705fb
Tyler Brown <tyler.s.brown.93@gmail.com>
2023-02-23 17:37:57 -0800

    initialize maxProfit

---
+++ maxProfit/maxProfit.js
@@ -0,0 +1,12 @@
+/*https://leetcode.com/problems/best-time-to-buy-and-sell-stock/*/
+
+/**
+ * @param {number[]} prices
+ * @return {number}
+*/
+
+const maxProfit = (prices) => {
+  // code
+}
+
+export default maxProfit;
---
+++ maxProfit/maxProfit.test.js
@@ -0,0 +1,1 @@
+import maxProfit from './maxProfit';
