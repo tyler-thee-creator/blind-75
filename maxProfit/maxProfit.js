51655afe98b39be1978502cd41e3e076adf80648
Tyler Brown <tyler.s.brown.93@gmail.com>
2023-02-27 16:11:08 -0800

    complete maxProfit

--- maxProfit/maxProfit.js
+++ maxProfit/maxProfit.js
@@ -6,7 +6,20 @@
 */
 
 const maxProfit = (prices) => {
-  // code
+  let buy = 0;
+  let sell = 1;
+  let profit = 0;
+
+  while (sell < prices.length) {
+    if (prices[sell] > prices[buy]) {
+      profit = Math.max(profit, prices[sell] - prices[buy])
+    } else {
+      buy = sell;
+    }
+    sell++;
+  }
+
+  return profit;
 }
 
 export default maxProfit;
