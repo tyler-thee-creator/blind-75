b3fd9c6255a748880c48b69907217ee2480c4fdb
Tyler Brown <tyler.s.brown.93@gmail.com>
2023-02-28 17:30:01 -0800

    initialize productExceptSelf

---
+++ productExceptSelf/productExceptSelf.js
@@ -0,0 +1,12 @@
+/*https://leetcode.com/problems/product-of-array-except-self/*/
+
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+*/
+
+const productExceptSelf = (nums) => {
+  // code
+}
+
+export default productExceptSelf;
