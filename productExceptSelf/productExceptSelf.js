79e1fb572d1ea7e915d44cf7766a82c13581f012
Tyler Brown <tyler.s.brown.93@gmail.com>
2023-03-02 20:37:50 -0800

    complete productExceptSelf

--- productExceptSelf/productExceptSelf.js
+++ productExceptSelf/productExceptSelf.js
@@ -6,7 +6,19 @@
 */
 
 const productExceptSelf = (nums) => {
-  // code
+  const result = new Array(nums.length).fill(1);
+  let productsLeft = 1;
+  let productsRight = 1;
+
+  for (let l = 0, r = nums.length - 1; l < nums.length; l++, r--) {
+    result[l] *= productsLeft;
+    result[r] *= productsRight;
+
+    productsLeft *= nums[l];
+    productsRight *= nums[r];
+  }
+
+  return result;
 }
 
 export default productExceptSelf;
