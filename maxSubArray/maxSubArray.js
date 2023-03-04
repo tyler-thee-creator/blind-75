078fcc4ce9b8737cf8f0ca48512d6c0d592ee2e3
Tyler Brown <tyler.s.brown.93@gmail.com>
2023-03-03 16:09:41 -0800

    initialize maxSubArray

---
+++ maxSubArray/maxSubArray.js
@@ -0,0 +1,12 @@
+/*https://leetcode.com/problems/maximum-subarray/*/
+
+/**
+ * @param {number[]} nums
+ * @return {number}
+*/
+
+const maxSubArray = (nums) => {
+  // code
+}
+
+export default maxSubArray;
