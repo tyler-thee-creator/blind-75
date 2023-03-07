308c9752f99dd95d327838db06cc36dfb14cc0ba
Tyler Brown <tyler.s.brown.93@gmail.com>
2023-03-06 17:36:41 -0800

    solve maxSubArray

--- maxSubArray/maxSubArray.js
+++ maxSubArray/maxSubArray.js
@@ -6,7 +6,14 @@
 */
 
 const maxSubArray = (nums) => {
-  // code
+  let sum = nums[0];
+
+  for (let i = 1; i < nums.length; i++) {
+      nums[i] = Math.max(nums[i], nums[i] + nums[i - 1]);
+      sum = Math.max(sum, nums[i]);
+  }
+
+  return sum;
 }
 
 export default maxSubArray;
